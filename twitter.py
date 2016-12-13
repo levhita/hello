230102ff2f2ff0c42d2700d3fc50b9af8a0d1869
Levhita <levhita@gmail.com>
2016-12-12 18:35:54 -0800

    Working for several friends at the same time

--- twitter.py
+++ twitter.py
@@ -1,28 +1,45 @@
 #Import the necessary methods from tweepy library
-from tweepy.streaming import StreamListener
+#from tweepy.streaming import StreamListener
+import tweepy
 from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 from tweepy import Stream
+
+#from tweepy import Stream
 import json
 import textwrap
 import ConfigParser
+import pprint
 
+lyrics = []
+counters = {}
+api = ""
+length = 0
+screen_name = ""
 
-#This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
+class MsgListener(StreamListener):
 
-    def on_data(self, data):
-        target = open("text.txt", 'w')
-        tweet = json.loads(data)
-        text = "@" + tweet['user']['screen_name'].encode('UTF-8')  + ": "  + tweet['text'].encode('UTF-8')
-	wrap = textwrap.fill(text, 50)
-        print wrap
-	target.write(wrap)        
-	target.close()	
-	return True
-
+    def on_direct_message(self, status):
+        global lyrics, counters, api, screen_name, length
+        
+        #pprint.pprint(vars(status))
+        from_screen_name = status._json['direct_message']['sender']['screen_name']
+        if from_screen_name != screen_name:
+            if not counters.has_key(from_screen_name):
+                counters[from_screen_name]=0
+            print counters[from_screen_name]
+            print screen_name 
+            print counters[from_screen_name]
+            api.send_direct_message(screen_name=from_screen_name, text=lyrics[counters[from_screen_name]])
+            if counters[from_screen_name] < length:
+                counters[from_screen_name] += 1
+            else:
+                counters[from_screen_name] = 0
+        
+        #print status._json['direct_message']['sender']['screen_name']
+        
     def on_error(self, status):
-        print status
-
+       print status
 
 if __name__ == '__main__':
 
@@ -33,13 +50,15 @@ if __name__ == '__main__':
     api_secret = Config.get("twitter", "api_secret")
     access_token = Config.get("twitter", "access_token")
     access_secret = Config.get("twitter", "access_secret")
+    screen_name = Config.get("twitter", "screen_name")
     
-    print api_key
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    #l = StdOutListener()
-    #auth = OAuthHandler(consumer_key, consumer_secret)
-    #auth.set_access_token(access_token, access_token_secret)
-    #stream = Stream(auth, l)
-
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    #stream.filter(track=[ '#hg40','hackergarage'])
+    songfile = Config.get("song", "filename")
+    lyrics = [line.rstrip('\n') for line in open(songfile)]
+    length = len(lyrics) - 1
+    
+    auth = OAuthHandler(api_key, api_secret)
+    auth.set_access_token(access_token, access_secret)
+    listener = MsgListener()
+    stream = Stream(auth, listener)
+    api = tweepy.API(auth)
+    stream.userstream()
